728f50f4bed1f8207be24687933d9c2fd38dceb6
sathishk <sathishkumar.thiyagarajan@gmail.com>
2025-01-04 20:14:44 +0530

    Hello JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,17 @@
 const path = require('path');
 
 module.exports = {
- // Your entry file
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true, // Cleans the output directory before each build
-  },
+  entry: './src/js/app.js',
   mode: 'development', // Set mode to 'development' for easier debugging
+  output: {
+  path: `${__dirname}/dist`,
+  filename: 'js/app.js',
+  },
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
     },
     port: 3000, // The port you want to use
     open: true, // Automatically opens the browser
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i, // Add loaders for CSS if needed
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
+  }
 };
