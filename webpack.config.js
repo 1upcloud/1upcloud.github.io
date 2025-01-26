5e07b7e68631ec75ccb77c08d0732e6100b085d6
sathishk <sathishkumar.thiyagarajan@gmail.com>
2025-01-26 08:57:57 +0530

    Removed Unused Images

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ module.exports = {
   entry: './src/js/app.js',
   mode: 'development', // Set mode to 'development' for easier debugging
   output: {
-  path: `${__dirname}/dist`,
-  filename: 'js/app.js',
+    path: `${__dirname}/dist`,
+    filename: 'js/app.js',
   },
   devServer: {
     static: {
